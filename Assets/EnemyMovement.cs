f5dc38b238b89dcf2baa6cf400c7c745c6d60ff7
PaulAntuono <ptantuono@yahoo.com>
2023-11-25 01:05:15 -0500

    slip material and enemy movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+/*
+    public gameObject pointA;
+    public gameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<RigidBody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    */
+}
+
+
