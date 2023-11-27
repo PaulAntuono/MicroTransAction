19080f65f9d254ba4a90fe2da2f569ec220ef741
PaulAntuono <ptantuono@yahoo.com>
2023-11-27 14:16:52 -0500

    Enemy changes

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-/*
-    public gameObject pointA;
-    public gameObject pointB;
+
+    public GameObject pointA;
+    public GameObject pointB;
     private Rigidbody2D rb;
-    private Animator anim;
     private Transform currentPoint;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>();
-        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         currentPoint = pointB.transform;
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
 
-    */
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
 }
 
 
