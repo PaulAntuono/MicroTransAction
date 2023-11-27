d4c3cbe8c0aebf9fe65095c75d1b054ac29d97bc
PaulAntuono <ptantuono@yahoo.com>
2023-11-27 12:13:12 -0500

    Fixed Bugs

--- Assets/UI/ScorePerSec.cs
+++ Assets/UI/ScorePerSec.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScorePerSec : MonoBehaviour
 {
-    public Text ScoreValueText;
+    public TMP_Text ScoreValueText;
     public float scoreValue = 0;
     public float scorePerSec = 1f;
 
